b45aa2f33bd6f2d1e610c484d8a5330cfdc07544
Csuhai Roland <roland.csuhai@4ig.hu>
2025-05-18 11:06:47 +0200

    fix placeholders

--- docker-run-from-github.ps1
+++ docker-run-from-github.ps1
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 --rm --name demo -it ghcr.io/SeniorRoland/cubix/cloudnative/demo:actions
+docker run -p 8080:8080 --rm --name demo -it ghcr.io/seniorroland/cubix/cloudnative/demo:actions
